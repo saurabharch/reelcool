9da7b80ccb61171deb261d08044c5838a5e7a6c7
Cristina <cristina.colon+git@gmail.com>
2015-10-01 12:31:40 -0400

    front-end video filters demo working

--- browser/js/play/play.js
+++ browser/js/play/play.js
@@ -1,12 +1,13 @@
-app.config(function ($stateProvider) {
+app.config(function($stateProvider) {
 
     $stateProvider.state('play', {
-        url: '/play/:source',
+        url: '/play',
         templateUrl: 'js/play/play.html',
         controller: 'PlayCtrl'
     });
 });
 
-app.controller('PlayCtrl', function ($scope, $state, $stateParams) {
-  $scope.source = $stateParams.source;
+app.controller('PlayCtrl', function($scope, $state, $stateParams) {
+    $scope.source = $stateParams.source;
+    angular.element(document).ready(init);  // calls init from video-effects-demo.js
 });
