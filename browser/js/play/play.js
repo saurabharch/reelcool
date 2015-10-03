56c3c1a2dcd2b33a24f98f6fdd99dff9edc7d362
Cristina <cristina.colon+git@gmail.com>
2015-10-03 13:03:14 -0400

    ng-init selects the video element every time, exclamation point

--- browser/js/play/play.js
+++ browser/js/play/play.js
@@ -10,22 +10,17 @@ app.config(function($stateProvider) {
 app.controller('PlayCtrl', function($scope, $state, InstructionsFactory) {
     $scope.videoSource = "dragon.ogg";
     $scope.instructions = InstructionsFactory.get();
-    // var video = document.getElementById("video");
-    // console.log(video);
     
     function changeVideo () {
     	$scope.videoSource = "IMG_2608.MOV"; // ostrich
     	video.load();
     }
 
-    // Still need to find a document.ready-type way to initialize this without setTimeout
-    // the document.ready method sometimes fires too early
-    // angular.element(document).ready(init); 
-    setTimeout(init,2000);
-
     var video;
-
-    function init(event) {
+    // We put init on the scope so that we call it with ng-init from play.html. 
+    // This makes the function able to select the video element when it loads. 
+    // Other methods (like document.ready) were unreliable, sometimes firing too early.
+    $scope.init = function () {
         video = document.getElementById("video");
         console.log(video);
         // setTimeout(changeVideo,3000); // change video source after 3 seconds
