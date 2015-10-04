fb52618f2f4858cff4bf0c5f3251589b044c6aa4
Kathy Lu <kaffy.lu@gmail.com>
2015-10-04 07:00:58 -0400

    slider almost works for clicking... but not when clip starts after start of its source video

--- browser/js/common/directives/slider/slider.js
+++ browser/js/common/directives/slider/slider.js
@@ -7,8 +7,57 @@ app.directive("timeSlider", () => {
       startTime: '=',
       endTime: '='
     },
-    link: (scope, element, attr) => {
+    link: (scope, elements, attr) => {
+      console.log("elements", elements);
+      var slider = elements[0];
+      console.log('slider', slider);
+      slider.style.opacity = "0";
 
+      slider.addEventListener('mouseenter', () => {
+        slider.style.opacity = '1';
+      })
+
+      slider.addEventListener('mouseleave', () => {
+        slider.style.opacity = '0';
+      })
+      // var sliderBg = document.getElementById('slider-bg');
+      // var sliderMercury = document.getElementById('slider-mercury');
+      var sliderDot = document.getElementById('slider-dot');
+      console.log('sliderDot', sliderDot);
+
+      // sliderDot.addEventListener('mousedown', () => {
+      //   //wherever the mouse goes, its x value should transfer to the totalCurrentTime
+      //   document.addEventListener('mousemove', (e) => {
+      //     var clickedX = e.clientX;
+      //     scope.movingTime = (clickedX - 140)/1120 * scope.endTime;
+      //     console.log("new moving Time", scope.movingTime);
+      //   })
+      // })
+
+      slider.addEventListener('click', (e) => {
+        //x value of coordinate  -> totalCurrentTime
+        console.log('clicked x', e.clientX);
+        var clickedX = e.clientX;
+        var newMovingTime = (clickedX - 140)/1120 * scope.endTime;
+        scope.$emit('newMovingTime', newMovingTime);
+        console.log("new moving Time", scope.movingTime);
+      })
+      // sliderBg.style.visibility = "hidden";
+      // sliderMercury.style.visibility = "hidden";
+      // sliderDot.style.visibility = "hidden";
+      // console.log("inside link", sliderBg, sliderMercury, sliderDot);
+      // sliderBg.addEventListener('mouseenter', (e)=> {
+      //   console.log("mouse entered");
+      //   sliderBg.style.visibility = "visible";
+      //   sliderMercury.style.visibility = "visible";
+      //   sliderDot.style.visibility = "visible";
+      // });
+      // sliderBg.addEventListener('mouseleave',(e)=> {
+      //   console.log("mouse left");
+      //   sliderBg.style.visibility = "hidden";
+      //   sliderMercury.style.visibility = "hidden";
+      //   sliderDot.style.visibility = "hidden";
+      // });
     }
   }
 });
