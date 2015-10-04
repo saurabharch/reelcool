a6ffe0063be66469815b332851b111c06a0d7cab
steve <spells24@gmail.com>
2015-10-03 20:01:42 -0400

    added slider that reflects video current play time on Playjs

--- browser/js/play/play.js
+++ browser/js/play/play.js
@@ -10,21 +10,57 @@ app.config(function($stateProvider) {
 app.controller('PlayCtrl', function($scope, $state, InstructionsFactory) {
     $scope.videoSource = "dragon.ogg";
     $scope.instructions = InstructionsFactory.get();
+    $scope.currentTime = 0;
+    var video = document.getElementById("video1");
     
-    function changeVideo () {
-    	$scope.videoSource = "IMG_2608.MOV"; // ostrich
-    	video.load();
-    }
 
-    var video;
+    video.onplaying = function(){
+        $scope.endTime=video.duration;
+        console.dir(video);
+        console.log($scope.endTime);
+        $scope.$digest();
+        var pointer = document.getElementsByClassName('rz-pointer')[0];
+        pointer.onmousedown = function(){
+            video.pause();
+            document.onclick = function(){
+                video.currentTime=Number(document.getElementsByClassName('rz-bubble')[2].innerHTML);
+                video.play();
+                document.onclick = null;
+            };
+            
+        };
+        
+    };
+    video.ontimeupdate = function(){
+        $scope.currentTime=video.currentTime;
+        $scope.$digest();
+    };
+
+    // video.load();
+    // video.play();
+    // $scope.endTime = video.duration;
+    // console.dir(video);
+    // console.log($scope.endTime)
+
+
+    function changeVideo () {
+        $scope.videoSource = "IMG_2608.MOV"; // ostrich
+        $scope.$digest();
+        console.dir(video);
+        video.load();
+    }
     // We put init on the scope so that we call it with ng-init from play.html. 
     // This makes the function able to select the video element when it loads. 
     // Other methods (like document.ready) were unreliable, sometimes firing too early.
-    $scope.init = function () {
-        video = document.getElementById("video");
-        console.log(video);
-        // setTimeout(changeVideo,3000); // change video source after 3 seconds
-    }
+
+
+    // video.ontimeupdate = function(){
+    //     console.log('thiscurrentTime', this.currentTime)
+    //     console.log('vidtime', video.currentTime)
+    //     $scope.currentTime=this.currentTime;
+    //     $scope.$digest();
+    // };
+    // setTimeout(changeVideo,5000); // change video source after 3 seconds
 
     // This is what we're considering for the instructions object. 
     // each time a user selects a new video source or changes a filter, 
