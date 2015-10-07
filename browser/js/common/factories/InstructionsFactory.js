df8d3b4bd168ff6edccd7b066465ebc251ac45bb
Kathy Lu <kaffy.lu@gmail.com>
2015-10-07 16:53:19 -0400

    filter toggling works

--- browser/js/common/factories/InstructionsFactory.js
+++ browser/js/common/factories/InstructionsFactory.js
@@ -2,13 +2,6 @@ app.factory('InstructionsFactory',function(){
 	var instructions = [];
 
 	return {
-		filters: [
-			{"CSSclass": "", "name":"No filter"},
-			{"CSSclass": "bw", "name":"Black & White"},
-			{"CSSclass": "sepia", "name":"Sepia"},
-			{"CSSclass": "blur", "name":"Blur"},
-			{"CSSclass": "invert", "name":"Invert"}
-		],
 		add: (instruction) => {
 			instructions.push(instruction);
 		},
--- browser/js/playground/playground.js
+++ browser/js/playground/playground.js
@@ -10,37 +10,47 @@ app.directive('playground', () => {
   }
 });
 
-app.controller('PlaygroundCtrl', ($scope, InstructionsFactory) => {
+app.controller('PlaygroundCtrl', ($scope, FilterFactory, InstructionsFactory) => {
+
+  var video;
+  var $video;
+
   $scope.run = () => {
-    $scope.appliedFilters = [];
-    var video = document.getElementsByTagName('video')[0];
+    $scope.filters = FilterFactory.filters.map(filter => {
+        filter.applied = false;
+        return filter;
+    });
+
+    video = document.getElementsByTagName('video')[0];
+    $video = $(video);
     $scope.duration = video.duration;
     $scope.startTime = 0;
     $scope.endTime = $scope.duration;
-    $scope.availableFilters = InstructionsFactory.filters;
   }
 
   $scope.toggleFilter = (filter) => {
-    var filterIndex = _.findIndex($scope.appliedFilters, (el) => {
-      return el.name === filter.name;
-    });
-    if(filterIndex>-1){
-      $scope.appliedFilters.splice(filterIndex);
-    }
-    else {
-      $scope.appliedFilters.push(filter);
-    }
-    console.log("applied",$scope.appliedFilters);
+
+    $scope.filters.forEach(el => {
+      if(filter.code ===""){
+        el.applied = false;
+      }
+      else if(el.code !== filter.code && el.type === filter.type && el.applied){
+        el.applied = false;
+      }
+    })
+
+    filter.applied = !filter.applied;
+    $scope.updateFilterString();
   }
 
-  $scope.updateFilters = () => {
-
+  $scope.updateFilterString = () => {
+    let newFilterStr = FilterFactory.createFilterString($scope.filters);
+    $video.attr('style', `-webkit-filter:${newFilterStr}`);
   }
 
   $scope.cutToInstructions = () => {
     console.log("cutToInstructions called")
     InstructionsFactory.add($scope.instructions);
-
   }
 
 });
