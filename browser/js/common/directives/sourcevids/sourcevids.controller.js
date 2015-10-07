2f2e309719111a2e50cd72aab823ff65320922f4
Daniel Moennich <daniel.moennich@gmail.com>
2015-10-07 11:11:33 -0400

    Added basic VideoFactory

--- browser/js/common/directives/sourcevids/sourcevids.controller.js
+++ browser/js/common/directives/sourcevids/sourcevids.controller.js
@@ -1,51 +1,71 @@
-app.controller("SourceVidsCtrl", function ($scope) {
+app.controller("SourceVidsCtrl", function ($scope, VideoFactory, IdGenerator) {
 
 	$scope.videos = [];
 
-	var videoIdCounter = 0;
+	var VideoElement = function (videoSource) {
+		this.id = IdGenerator();
+		this.videoSource = videoSource;
+	};
+
+
+	//var videoIdCounter = 0;
 
 	var fileInput = document.getElementById("videofileinput");
 
-	// if new file selected
-	// read file
-	// create new MediaSource and append source
-	// when done, add to list
 	fileInput.addEventListener('change', function(e) {
 		var file = fileInput.files[0];
-		var videoId = "video_" + videoIdCounter++;
-		var mediaSource = new MediaSource();
-		var objUrl = window.URL.createObjectURL(mediaSource);
-		var videoObj = {
-			id: videoId,
-			objUrl: objUrl
-		};
-		$scope.videos.push(videoObj);
-		$scope.$digest();
-		var video = document.querySelector('#' + videoId);
-		video.addEventListener("seeked", function (e) {
-			console.log("readystate changed:" , e);
+		VideoFactory.addVideoSource(file).then(function (videoSource) {
+			console.log("Video source received:", videoSource);
+			var videoElement = new VideoElement(videoSource);
+			$scope.videos.push(videoElement);
+			$scope.$digest();
+			return VideoFactory.attachVideoSource(videoSource, videoElement.id);
+		}).then(function () {
+			console.log("Video source attached to video element");
+		}).then(null, function (error) {
+			console.error("Error occured when attaching video source", error);
 		});
-		//var video = document.querySelector('#' + videoId);
-		video.src = objUrl;
-		addVideoToList(file, mediaSource, videoObj);
+		// var videoId = "video_" + videoIdCounter++;
+		// var mediaSource = new MediaSource();
+		// var objUrl = window.URL.createObjectURL(mediaSource);
+		// var videoObj = {
+		// 	id: videoId,
+		// 	objUrl: objUrl
+		// };
+		// $scope.videos.push(videoObj);
+		// $scope.$digest();
+		// var video = document.querySelector('#' + videoId);
+		// video.addEventListener("seeked", function (e) {
+		// 	console.log("readystate changed:" , e);
+		// });
+		// //var video = document.querySelector('#' + videoId);
+		// video.src = objUrl;
+		// addVideoToList(file, mediaSource, videoObj);
     });
 
+	var createVideoElement = function (videoSource) {
 
-	$scope.onmain = function (arrayBuffer) {
-		var mediaSource = new MediaSource();
-		mediaSource.addEventListener("sourceopen", function () {
-			console.log("source open");
-	        var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-	        console.log("???");
-	        sourceBuffer.addEventListener('updateend', function(_) {
-	            mediaSource.endOfStream();
-	        });
-	        console.log("???????????");
-	        sourceBuffer.appendBuffer(arrayBuffer);
-		});
-		var objUrl = window.URL.createObjectURL(mediaSource);
-		var video = document.getElementById('mainplayer');
-		video.src = objUrl;
+
+	};
+
+
+
+	$scope.onmain = function (videoElement) {
+		VideoFactory.attachVideoSource(videoElement.videoSource, "mainplayer");
+		// var mediaSource = new MediaSource();
+		// mediaSource.addEventListener("sourceopen", function () {
+		// 	console.log("source open");
+	 //        var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+	 //        console.log("???");
+	 //        sourceBuffer.addEventListener('updateend', function(_) {
+	 //            mediaSource.endOfStream();
+	 //        });
+	 //        console.log("???????????");
+	 //        sourceBuffer.appendBuffer(arrayBuffer);
+		// });
+		// var objUrl = window.URL.createObjectURL(mediaSource);
+		// var video = document.getElementById('mainplayer');
+		// video.src = objUrl;
 
 	};
 
---
+++ browser/js/common/factories/VideoFactory.js
@@ -0,0 +1,63 @@
+app.factory("VideoFactory", function (IdGenerator) {
+
+	var vidFactoy = {},
+		videoSources = [];
+
+
+	var VideoSource = function (fileName, arrayBuffer) {
+		this.id = IdGenerator();
+		this.fileName = fileName;
+		this.arrayBuffer = arrayBuffer;
+	};
+
+	// do ajax polling for uplodaed videos
+	// load new video + create VideoObject
+	// and add new videos to list + send event of new videos
+
+
+	vidFactoy.getVideoSources = function () {
+		return videoSources;
+	};
+
+
+	vidFactoy.addVideoSource = function(file) {
+		return new Promise(function (resolve, reject) {
+			var reader = new FileReader();
+			console.log(file);
+			console.log("file size:", Math.round(file.size / 1000) / 1000, "MB");
+			reader.onloadend = function() {
+				console.log("reading file finished");
+				var videoSrc = new VideoSource(file.name, reader.result);
+				videoSources.push(videoSrc);
+				// TODO emit event about new video
+				resolve(videoSrc);
+			};
+			reader.readAsArrayBuffer(file);
+		});
+	};
+
+
+	//TODO maybe attack info about which video elements are using a videoObj
+	// and the mediasource objects
+	vidFactoy.attachVideoSource = function (videoSource, videoElementId) {
+		return new Promise(function (resolve, reject) {
+			var mediaSource = new MediaSource();
+			mediaSource.addEventListener("sourceopen", function () {
+				console.log("source open");
+				var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+				sourceBuffer.addEventListener('updateend', function(_) {
+					mediaSource.endOfStream();
+					resolve();
+				});
+				sourceBuffer.appendBuffer(videoSource.arrayBuffer);
+			});
+			var objUrl = window.URL.createObjectURL(mediaSource);
+			var video = document.getElementById(videoElementId);
+			video.src = objUrl;
+		});
+	};
+
+
+	return vidFactoy;
+
+});
