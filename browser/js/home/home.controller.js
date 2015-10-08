91ae8e2ab2552b7a8e0467819ad562699f71d7f0
steve <spells24@gmail.com>
2015-10-08 13:12:22 -0400

    can convert all files to mp4 using child processes

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -17,11 +17,41 @@ app.controller('homeCtrl', function ($http, $scope, $state, VideoFactory){
 	            enctype:'multipart/form-data',
 	            data: formData,
 	            processData:false,
-	            contentType:false
+	            contentType:false,
 	        }).done(function(data){
 	        	console.log('done!');
 	        });
 
+	};
+
+	$scope.download = function(){
+		console.log('yo');
+		$.ajax({
+			method: 'POST',
+			url:'/api/videos/download',
+			beforeSend: function(jqXHR,settings){
+				jqXHR.setRequestHeader("Connection", "Keep-Alive");
+			},
+			data:  {data:[
+			{
+				startTime: 2,
+				endTime: 3,
+				filters: ["blur"]
+			},
+			{
+				startTime: 4,
+				endTime: 6,
+				filters: ["sepia"]
+			},
+			{
+				startTime: 3,
+				endTime: 4,
+				filters: ["grayscale"]
+			}
+			]}
+		}).done(function(vid){
+			console.log('downloaded that shit!');
+		});
 	};
 	
 });
