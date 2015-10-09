61f0312a8c27fa5c82037890c86ed8beb2015e1e
Cristina <cristina.colon+git@gmail.com>
2015-10-09 17:23:33 -0400

    removed form that was no longer in use

--- browser/js/home/home.controller.js
+++ browser/js/home/home.controller.js
@@ -1,30 +1,4 @@
 app.controller('homeCtrl', function ($http, $scope, $state, VideoFactory){
-	var theFileInput = document.getElementById('fileupload');
-
-	$scope.upload = function(){
-
-
-	    var file = theFileInput.files[0];
-	    var reader = new FileReader();
-
-	    // reader.readAsArrayBuffer(file);
-	    var formData = new FormData();
-	    formData.append("uploadedFile",file);
-
-	    $.ajax({
-	            method: 'POST',
-	            url: '/api/videos/upload',
-	            enctype:'multipart/form-data',
-	            data: formData,
-	            processData:false,
-	            contentType:false,
-	        }).done(function(data){
-	        	console.log('done!');
-	        });
-
-	};
-
-
 	//temporarily setting instructions here to test the playground
 	$scope.instructions = [{
 		source: 'ost.ogv',
