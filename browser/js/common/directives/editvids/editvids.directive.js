fa4a039cf23ec7c1167915784c6780bf408dd06b
Daniel Moennich <daniel.moennich@gmail.com>
2015-10-10 15:26:55 -0400

    starting edit panel

---
+++ browser/js/common/directives/editvids/editvids.directive.js
@@ -0,0 +1,15 @@
+app.directive("editvids", function () {
+	return {
+		restrict: "E",
+		templateUrl: "js/common/directives/editvids/editvids.html",
+		link: function (scope, element, attr) {
+
+
+
+
+		}
+
+	};
+});
+
+
