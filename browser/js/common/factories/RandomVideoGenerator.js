bb09748010d79632776085cad73c1f09cf2382f6
Cristina <cristina.colon+git@gmail.com>
2015-10-13 12:54:39 -0400

    now able to edit clips that came from RandomVideoGenerator

--- browser/js/common/factories/RandomVideoGenerator.js
+++ browser/js/common/factories/RandomVideoGenerator.js
@@ -25,7 +25,7 @@ app.factory("RandomVideoGenerator", function (FilterFactory, InstructionsFactory
 			i;
 
 		for (i = 0; i < cutsNumber; i++) {
-			instr = InstructionsFactory.generate(video.videoSource, duration);
+			instr = InstructionsFactory.generate(video.videoSource, duration, true);
 			range = getRange(duration, cutLength);
 			instr.startTime = range[0];
 			instr.endTime = range[1];
