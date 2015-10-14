4a16fb8fa381cfb1207538da1ac290bb3b6d7989
Cristina <cristina.colon+git@gmail.com>
2015-10-14 09:38:42 -0400

    config for larger monitor

--- browser/js/app.js
+++ browser/js/app.js
@@ -17,6 +17,11 @@ app.config(function ($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
 });
 
+// app.config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('default')
+//     .dark();
+// });
+
 // This app.run is for controlling access to specific states.
 app.run(function ($rootScope, AuthService, $state) {
 
