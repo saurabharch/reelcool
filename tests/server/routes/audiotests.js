ded072a5cfb0ab60d6db7086ad167cec66e2d834
Daniel Moennich <daniel.moennich@gmail.com>
2015-10-18 19:14:51 -0400

    added more test helper

--- tests/server/routes/audiotests.js
+++ tests/server/routes/audiotests.js
@@ -7,6 +7,7 @@ var loggedInUserAgent = require('../../helper/loggedInAgent');
 var path = require("path");
 var fs = require("fs");
 var remUserDir = require("../../helper/remUserDir");
+var doesFileExist = require("../../helper/doesFileExist");
 
 var dbURI = 'mongodb://localhost:27017/reelTestingDB';
 var clearDB = require('mocha-mongoose')(dbURI);
@@ -18,102 +19,145 @@ var app = require('../../../server/app');
 
 describe('The Audio route', function() {
 
-	beforeEach('Establish DB connection', function(done) {
-		if (mongoose.connection.db) return done();
-		mongoose.connect(dbURI, done);
-	});
+  beforeEach('Establish DB connection', function(done) {
+    if (mongoose.connection.db) return done();
+    mongoose.connect(dbURI, done);
+  });
 
-	afterEach('Clear test database', function(done) {
-		clearDB(done);
-	});
+  afterEach('Clear test database', function(done) {
+    clearDB(done);
+  });
 
-	afterEach("Remove user directories", function () {
-		return remUserDir();
-	});
+  afterEach("Remove user directories", function() {
+    return remUserDir();
+  });
 
-	var loggedInAgent;
+  var loggedInAgent;
 
-	beforeEach('Create loggedIn user agent and authenticate', function() {
-		return loggedInUserAgent()
-			.then(function(userAgent) {
-				loggedInAgent = userAgent;
-			});
-	});
-
-
-	it('should allow uploading audio files', function(done) {
-
-		loggedInAgent
-		.post("/api/audio/upload")
-		.attach("uploadedFile", path.join(__dirname, "../../fixtures/audiotest.mp3"))
-		.expect(201)
-		.end(function (err, res) {
-			var fileName = res.text;
-
-			fs.accessSync(path.join(__dirname, "../../../server/files/" +
-				loggedInAgent.mongoId + "/uploaded/" + fileName + ".mp3"), fs.F_OK);
+  beforeEach('Create loggedIn user agent and authenticate', function() {
+    return loggedInUserAgent()
+      .then(function(userAgent) {
+        loggedInAgent = userAgent;
+      });
+  });
 
 
-			Audio.findById(fileName).exec()
-			.then(function (audio) {
-				expect(audio.title).to.equal("audiotest");
-				expect(audio.editor.toString()).to.equal(loggedInAgent.mongoId.toString());
-				expect(audio.theme).to.equal(false);
-				done();
-			}).then(null, done);
+  describe("for POST", function() {
+
+    it('should create a DB entry with audio file title and editor', function(done) {
+      loggedInAgent
+        .post("/api/audio/upload")
+        .attach("uploadedFile", path.join(__dirname, "../../fixtures/audiotest.mp3"))
+        .expect(201)
+        .end(function(err, res) {
+          var fileName = res.text;
+          Audio.findById(fileName).exec()
+            .then(function(audio) {
+              expect(audio.title).to.equal("audiotest");
+              expect(audio.editor.toString()).to.equal(loggedInAgent.mongoId.toString());
+              expect(audio.theme).to.equal(false);
+              done();
+            }).then(null, done);
+        });
+
+    });
 
 
-		});
+    it('should put the uploaded file in the users uploaded directory', function(done) {
+      loggedInAgent
+        .post("/api/audio/upload")
+        .attach("uploadedFile", path.join(__dirname, "../../fixtures/audiotest.mp3"))
+        .expect(201)
+        .end(function(err, res) {
+			var fileName = res.text,
+				fileExists = doesFileExist(loggedInAgent.mongoId, "uploaded", fileName + ".mp3");
+          expect(fileExists).to.equal(true);
+          done();
+        });
+    });
 
-	});
-
-
-
-
-	it('should allow deleting audio files', function(done) {
-
-		var useDeleteRoute = function (audioMongoId) {
-			loggedInAgent
-			.delete("/api/audio/" + audioMongoId)
-			.end(function (err, res) {
-
-				try {
-					fs.statSync(path.join(__dirname, "../../../server/files/" +
-						loggedInAgent.mongoId + "/uploaded/" + audioMongoId + ".mp3"));
-					expect(true).to.be(false);
-				} catch (e) {
-					// catch expected error, since file should not be there anymore
-				}
-
-				//console.log("FSTAT:", fStat);
-
-				Audio.findById(audioMongoId).exec()
-				.then(function (audio) {
-					expect(audio).to.equal(null);
-					done();
-				}).then(null, done);
-
-			});
-		};
+  });
 
 
-		// create audio entry in DB
-		Audio.create({title: "sample", editor: loggedInAgent.mongoId})
-		.then(function (audio) {
 
-			// cp audio file in user dir
-			var sourcePath = path.join(__dirname, "../../fixtures/audiotest.mp3"),
-				destPath = path.join(__dirname, "../../../server/files/" +
-					loggedInAgent.mongoId + "/uploaded/sample.mp3");
-			fs.createReadStream(sourcePath).pipe(fs.createWriteStream(destPath));
+  describe("for DELETE", function() {
 
-			// using the route to delete the audio file
-			useDeleteRoute(audio._id);
-		});
+    var audioMongoId;
+
+    beforeEach("give the user an audio file", function() {
+      // create audio entry in DB
+      console.log("mongoooooID", loggedInAgent.mongoId);
+      return Audio.create({
+          title: "sample",
+          editor: loggedInAgent.mongoId
+        })
+        .then(function(newAudio) {
+
+          // cp audio file in user dir
+          var sourcePath = path.join(__dirname, "../../fixtures/audiotest.mp3"),
+            destPath = path.join(__dirname, "../../../server/files/" +
+              loggedInAgent.mongoId + "/uploaded/" + newAudio._id + ".mp3");
+          fs.createReadStream(sourcePath).pipe(fs.createWriteStream(destPath));
+
+          audioMongoId = newAudio._id;
+        });
+    });
 
 
-	});
+    it('should remove the corresponding file from the users directory', function(done) {
+
+      // file should exist before deletion
+      var fileExists = doesFileExist(loggedInAgent.mongoId, "uploaded",
+        audioMongoId + ".mp3");
+      expect(fileExists).to.equal(true);
+
+      loggedInAgent
+        .delete("/api/audio/" + audioMongoId)
+        .end(function(err, res) {
+
+          fileExists = doesFileExist(loggedInAgent.mongoId, "uploaded",
+            audioMongoId + ".mp3");
+          expect(fileExists).to.equal(false);
+          done();
+
+        });
+
+    });
+
+
+
+
+    it('should remove the corresponding entry in the DB', function(done) {
+
+      var callDelete = function() {
+        loggedInAgent
+          .delete("/api/audio/" + audioMongoId)
+          .end(function(err, res) {
+            Audio.findById(audioMongoId).exec()
+              .then(function(audio) {
+                expect(audio).to.equal(null);
+                done();
+              }).then(null, done);
+          });
+      };
+
+      // the DB entry should exist before deletion
+      Audio.findById(audioMongoId).exec()
+        .then(function(audio) {
+          expect(audio).not.to.equal(null);
+          callDelete();
+        }).then(null, done);
+
+
+    });
+
+
+
+
+  });
 
 
 
 });
+
+
